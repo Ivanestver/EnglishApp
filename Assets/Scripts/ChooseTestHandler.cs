e8f21f1b28e815a1ca5059d6b791b11e34b4ea77
IPGamer <bla@bla.com>
2022-04-29 01:26:20 +0300

    commit everything left

---
+++ Assets/Scripts/ChooseTestHandler.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChooseTestHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject wordGO;
+    [SerializeField] private Transform content;
+
+    public static Test SelectedTest = null;
+
+    private void Start()
+    {
+        var tests = ChooseThemeController.SelectedTheme.GetTestNames();
+        if (tests.Count == 0)
+            return;
+
+        for (int i = 0; i < tests.Count; i++)
+        {
+            var word = Instantiate(wordGO, content);
+            word.GetComponentInChildren<Text>().text = tests[i];
+            word.GetComponent<Button>().onClick.AddListener(OnTestStart);
+        }
+    }
+
+    private void OnTestStart()
+    {
+        var currentButton = EventSystem.current.currentSelectedGameObject;
+        SelectedTest = ChooseThemeController.SelectedTheme.GetTestByTestName(
+            currentButton.GetComponentInChildren<Text>().text);
+
+        SceneManager.LoadScene(4);
+    }
+
+    public void BackToChooseTheme()
+    {
+        SelectedTest = null;
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ Assets/Scripts/ChooseThemeController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChooseThemeController : MonoBehaviour
+{
+    [SerializeField] private Transform content;
+    [SerializeField] private GameObject wordGO;
+    [SerializeField] private Text welcomeText;
+    [SerializeField] private Storage storage;
+
+    public static Theme SelectedTheme = null;
+
+    private void OnEnable()
+    {
+        var tests = WordsStorage.GetAllThemes();
+        if (tests.Count == 0)
+            return;
+
+        for (int i = 0; i < tests.Count; i++)
+        {
+            var word = Instantiate(wordGO, content);
+            word.GetComponentInChildren<Text>().text = tests[i];
+            word.GetComponent<Button>().onClick.AddListener(MoveToChoose);
+        }
+
+        welcomeText.text = $"Здравствуйте, {storage.StudentName}. Выберите тему";
+    }
+    
+    private void MoveToChoose()
+    {
+        var selectedButton = EventSystem.current.currentSelectedGameObject;
+        SelectedTheme = WordsStorage.GetThemeByName(selectedButton.GetComponentInChildren<Text>().text);
+        PersonalDataHandler.IsValidated = true;
+        SceneManager.LoadScene(7);
+    }
+
+    public void BackToMenu()
+    {
+        PersonalDataHandler.IsValidated = false;
+        SceneManager.LoadScene(0);
+    }
+}
